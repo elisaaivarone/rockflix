26a2cb5c9ae3c37b418614420e9328572c9035f6
elisaaivarone <lisa.sandrade@yahoo.com.br>
2020-08-02 03:44:54 -0300

    Implementando eslint e css do form

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -2,112 +2,90 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PagesDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '',
-  }
+  };
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
-
 
   function setValue(chave, valor) {
     setValues({
       ...values,
       [chave]: valor,
-    })
+    });
   }
 
   function handleChange(infosDoEvento) {
     setValue(
       infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value
+      infosDoEvento.target.value,
     );
   }
 
   return (
-  <PageDefault>
-    <h1>Cadastro de Categoria: {values.nome}</h1>
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
-    <form onSubmit={function handleSubmit(infosEvento) {
-      infosEvento.preventDefault();
+      <form onSubmit={function handleSubmit(infosEvento) {
+        infosEvento.preventDefault();
 
-      setCategorias([
-        ...categorias,
-        values
-      ]);
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
 
-      setValues(valoresIniciais)
-       
-    }}>
-      
-    <FormField 
-      label="Nome da Categoria"
-      type="text"
-      name="nome"
-      value={values.nome}
-      onChange={handleChange} />
-    
-    <FormField 
-      label="Descrição"
-      type="text"
-      name="descricao"
-      value={values.descricao}
-      onChange={handleChange} />
-    {/*<div>
-      <label>
-        Descrição:
-        <textarea
+        setValues(valoresIniciais);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria"
           type="text"
-          value={values.descricao}
-          name='descricao'
+          name="nome"
+          value={values.nome}
           onChange={handleChange}
         />
-      </label>
-    </div>*/}
-        
-    <FormField 
-      label="Cor"
-      type= "color"
-      name= "cor"
-      value={values.cor}
-      onChange={handleChange}
-    />  
-    {/*<div>
-      <label>
-        Cor:
-        <input
-          type="color"
-          value={values.cor}
-          name='cor'
-          onChange={handleChange}
-        />
-      </label>
-    </div>*/}
-      
-      <button>
-        Cadastrar
-      </button>
-    </form>
 
-    <ul>
-      {categorias.map((categoria, indice) => {
-        return (
-          <li key={`${categoria}${indice}`}>
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
             {categoria.nome}
           </li>
-        )
-      })}
-    </ul>
+        ))}
+      </ul>
 
-
-  <Link to="/">
-    Ir para home
-    </Link>
-  </PageDefault>
-  )
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Menu from '../../components/Menu'
+import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BarnnerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
@@ -23,37 +23,37 @@ function Home() {
   return (
     <AppWrapper>
       <Menu />
-      <BarnnerMain 
+      <BarnnerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"Ao melhor cantor do Brasil, Andre Matos!"}
+        videoDescription={'Ao melhor cantor do Brasil, Andre Matos!'}
       />
 
-      <Carousel 
+      <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
       />
 
-      <Carousel 
-        category={dadosIniciais.categorias[1]} 
+      <Carousel
+        category={dadosIniciais.categorias[1]}
       />
 
-      <Carousel 
-        category={dadosIniciais.categorias[2]} 
+      <Carousel
+        category={dadosIniciais.categorias[2]}
       />
 
-      <Carousel 
-        category={dadosIniciais.categorias[3]} 
+      <Carousel
+        category={dadosIniciais.categorias[3]}
       />
 
-      <Carousel 
-        category={dadosIniciais.categorias[4]} 
+      <Carousel
+        category={dadosIniciais.categorias[4]}
       />
 
-      <Carousel 
-        category={dadosIniciais.categorias[5]} 
+      <Carousel
+        category={dadosIniciais.categorias[5]}
       />
-      
+
       <Footer />
     </AppWrapper>
   );
